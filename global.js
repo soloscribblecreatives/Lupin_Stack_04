888ba5dccc02c121deaf3b69547f6d7a076781ed
Solo Scribble Creatives <34957574+soloscribblecreatives@users.noreply.github.com>
2021-12-21 02:08:59 +0530

    Write

--- global.js
+++ global.js
@@ -198,7 +198,7 @@ if (typeof(localStorage.getItem('currentslide'))!='undefined' && localStorage.ge
 	
  } 
  
-	if(nextSlideNo <= 4){//number 3 is number of total slides present
+	if(nextSlideNo <= 3){//number 3 is number of total slides present
 	// alert(nextSlideNo);
 	var tempNext = localStorage.getItem(currentContentId+"_"+contentName+"_slideNo_"+nextSlideNo);
 
@@ -302,10 +302,10 @@ if(direction == 'b') {
 //custom slide changes ends here....
 
 	else{
-	if(page_id <= 4){
+	if(page_id <= 3){
 		page_id = page_id + 1;
 		//alert(page_id);
-		if(page_id == 5){
+		if(page_id == 4){
             flag=1;
         }
 	}
@@ -388,13 +388,10 @@ switch(pg_id){
 	content='<link rel="stylesheet" type="text/css" href="slide1/slide1.css" media="screen"/><div class="background"><img src="slide1/1.jpg" width="1024" height="768" alt=""></div>';
 	break;
 	case 2:
-	content='<link rel="stylesheet" type="text/css" href="slide2/slide2.css" media="screen"/><div class="background"><img src="slide2/1.jpg" width="1024" height="768" alt=""></div>';
+	content='<link rel="stylesheet" type="text/css" href="slide1/slide1.css" media="screen"/><div class="background"><img src="slide2/1.jpg" width="1024" height="768" alt=""></div>';
 	break;
     case 3:
-	content='<link rel="stylesheet" type="text/css" href="slide3/slide3.css" media="screen"/><div class="background"><img src="slide3/1.jpg" width="1024" height="768" alt=""></div>';
-	break;
-    case 4:
-	content='<link rel="stylesheet" type="text/css" href="slide4/slide4.css" media="screen"/><div class="background"><img src="slide4/1.jpg" width="1024" height="768" alt=""></div>';
+	content='<link rel="stylesheet" type="text/css" href="slide3/slide3.css" media="screen"/><iframe src="slide3/write.html" width="1024" height="768" frameBorder="0" title="W3Schools Free Online Web Tutorials"></iframe>';
 	break;
 }
 
@@ -536,3 +533,87 @@ $(document).ready(function(){
 		$('.touchbtn').css("display","block");
 	})
 })
+
+
+
+
+window.onload = function () {
+
+  // Definitions
+  var canvas = document.getElementById("paint-canvas");
+  var context = canvas.getContext("2d");
+  var boundings = canvas.getBoundingClientRect();
+
+  // Specifications
+  var mouseX = 0;
+  var mouseY = 0;
+  context.strokeStyle = 'black'; // initial brush color
+  context.lineWidth = 1; // initial brush width
+  var isDrawing = false;
+
+
+  // Handle Colors
+  var colors = document.getElementsByClassName('colors')[0];
+
+  colors.addEventListener('click', function(event) {
+    context.strokeStyle = event.target.value || 'black';
+  });
+
+  // Handle Brushes
+  var brushes = document.getElementsByClassName('brushes')[0];
+
+  brushes.addEventListener('click', function(event) {
+    context.lineWidth = event.target.value || 1;
+  });
+
+  // Mouse Down Event
+  canvas.addEventListener('mousedown', function(event) {
+    setMouseCoordinates(event);
+    isDrawing = true;
+
+    // Start Drawing
+    context.beginPath();
+    context.moveTo(mouseX, mouseY);
+  });
+
+  // Mouse Move Event
+  canvas.addEventListener('mousemove', function(event) {
+    setMouseCoordinates(event);
+
+    if(isDrawing){
+      context.lineTo(mouseX, mouseY);
+      context.stroke();
+    }
+  });
+
+  // Mouse Up Event
+  canvas.addEventListener('mouseup', function(event) {
+    setMouseCoordinates(event);
+    isDrawing = false;
+  });
+
+  // Handle Mouse Coordinates
+  function setMouseCoordinates(event) {
+    mouseX = event.clientX - boundings.left;
+    mouseY = event.clientY - boundings.top;
+  }
+
+  // Handle Clear Button
+  var clearButton = document.getElementById('clear');
+
+  clearButton.addEventListener('click', function() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+  });
+
+  // Handle Save Button
+  var saveButton = document.getElementById('save');
+
+  saveButton.addEventListener('click', function() {
+    var imageName = prompt('Please enter image name');
+    var canvasDataURL = canvas.toDataURL();
+    var a = document.createElement('a');
+    a.href = canvasDataURL;
+    a.download = imageName || 'drawing';
+    a.click();
+  });
+};
